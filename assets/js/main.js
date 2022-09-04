a69b5b3db44fd773f1be5cb40e51512d49b05eb6
V-Svistunova <wika1706@gmail.com>
2022-09-04 19:49:33 +0300

    feat: add app weather

--- assets/js/main.js
+++ assets/js/main.js
@@ -129,7 +129,7 @@ function swiperCard() {
         breakpoints: {
           320: {
             slidesPerView: 1,
-            spaceBetween: 20
+            spaceBetween: 20,
           },
           400: {
             slidesPerView: 2,
@@ -151,6 +151,55 @@ swiperCard();
 window.addEventListener("resize", swiperCard);
 
 
+// Виджет погода
+
+const weatherBlock = document.querySelector('#weather');
+
+async function loadWeather(e) {
+  weatherBlock.innerHTML = `
+    <div class="weather__loading">
+      <img src="assets/img/loading.gif" alt="loading...">
+    </div> `; 
+    
+    const server = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=Minsk&appid=39f92d725f68d92be0f20fced6841713'
+    const response = await fetch(server, {
+      method: 'GET',
+    });
+    const responseResult = await response.json();
+
+    if(response.ok) {
+      getWeater(responseResult);
+    }else {
+      weatherBlock.innerHTML = responseResult.message;
+    }
+  }
+
+  function getWeater(data){
+    const location = data.name;
+    const temp = Math.round(data.main.temp);
+    const feelsLike = Math.round(data.main.feels_like);
+    const weatherStatus = data.weather[0].main;
+    const weatherIcon = data.weather[0].icon;
+
+    const template = `
+      <div class="weather__header">
+        <div class="weather__main">
+          <div class="weather__city">${location}</div>
+          <div class="weather__status">${weatherStatus}</div>
+        </div>
+        <div class="weather__icon">
+          <img src="https://openweathermap.org/img/w/${weatherIcon}.png" alt="${weatherStatus}">
+        </div>
+      </div>
+      <div class="weather__temp">${temp}</div>
+      <div class="weather__feels-like">Feels like: ${feelsLike}</div>`;
+
+      weatherBlock.innerHTML = template;
+    }
+
+  if(weatherBlock) {
+    loadWeather();
+  }
 
 
 
